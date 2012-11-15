760c03480e6f2d245a85962c717be245ef60beaa
moro <moronatural@gmail.com>
2012-11-15 11:58:47 +0900

    bundle exec rails new hobbykaigi2013 -T -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hobbykaigi2013::Application.config.secret_token = '0be662b64a0d743602f101af405384925d7982b5572da73dbcd5e4e228169a735d3ae5083a0670a91fd4b835781b787df83f5ebc90cf8e701a76c7bbd13da963'
