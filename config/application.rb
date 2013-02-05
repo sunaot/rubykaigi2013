e80bc00cd43c349ae97da8b436f19865407e567d
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-02-05 20:27:02 +0900

    Serve assets with prefix '/2013'

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,7 @@ module RubyKaigi2013
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.prefix = '/2013'
   end
 end
