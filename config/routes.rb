f8249af022c2b681558806ce84e5784c5713b260
SHIMADA Koji <koji.shimada@enishi-tech.com>
2013-03-05 11:07:56 +0900

    [WIP] Add accommodation.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 RubyKaigi2013::Application.routes.draw do
+  get "pages/accommodation"
+
   scope '2013' do
     root to: 'top#show'
+    get 'accommodation' => 'pages#accommodation'
     resources :talks, only: %w[index show]
   end
 end
