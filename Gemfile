976da34f964102ab8fd78cf46651433890977042
Keita Urashima <ursm@ursm.jp>
2013-03-12 22:24:30 +0900

    Migrate to Middleman

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+
+gem 'middleman', '~> 3.0.12'
+gem 'haml'
+
+gem 'rb-inotify'
---
+++ config.rb
@@ -0,0 +1,11 @@
+activate :directory_indexes
+
+set :css_dir,    '2013/stylesheets'
+set :js_dir,     '2013/javascripts'
+set :images_dir, '2013/images'
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  activate :asset_hash
+end
