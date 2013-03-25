515df873354a0f54c31f15c0721839b42fe1428c
Ryunosuke SATO <tricknotes.rs@gmail.com>
2013-03-25 13:26:29 +0900

    Ignore asset hash for banner and badge

--- config.rb
+++ config.rb
@@ -7,5 +7,5 @@ set :images_dir, '2013/images'
 configure :build do
   activate :minify_css
   activate :minify_javascript
-  activate :asset_hash
+  activate :asset_hash, ignore: [%r{^2013/images/(?:banner|badge)}]
 end
